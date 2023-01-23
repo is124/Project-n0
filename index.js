6284f4b735c2c96b7e494df1c03bd9674288f526
is124 <tilaks484@gmail.com>
2023-01-23 11:59:05 +0530

    cors, body-parser added

--- index.js
+++ index.js
@@ -2,7 +2,18 @@ const express = require("express");
 const app = express();
 const PORT = 8081;
 const Routes = require("./Routers");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
+const corsOptions = {
+  origin: "http://www.localhost:3000/", //base origin from where requests will come
+  credentials: true, // access-control-allow-credentials: true
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use("/v1", Routes);
 
 app.listen(PORT, () => {
